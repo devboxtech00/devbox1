e827ffed4c77230d1369081723c936d951f4434a
devboxtech00 <141001979+devboxtech00@users.noreply.github.com>
2023-08-15 19:52:54 +0530

    Update header.php

--- miss/dummytheme/header.php
+++ miss/dummytheme/header.php
@@ -23,3 +23,25 @@
 				)
 			);
 			?>
+
+
+                <?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+                  $count = WC()->cart->cart_contents_count;
+                ?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php 
+                 if ( $count > 0 ) {
+                 ?>
+                
+                 <span class="cart-item"><?php echo esc_html( $count ); ?></span>
+                 <?php
+                    }
+                 ?></a>
+               <?php } ?>
+
+
+
+    <form role="search" method="get"  action="<?php echo home_url( '/' );?>">
+              <div class="form-input">
+                <input type="text" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" required>
+                <input type="submit" value="SUBMIT"> 
+              </div>
+            </form>
